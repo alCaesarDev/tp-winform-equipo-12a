2aa9088ddb4174d52264795ae8cde713d97b7441
alCaesar <agustins379@gmail.com>
2025-09-06 12:25:39 -0300

    first push

---
+++ Infraestructura/BaseDeDatos.cs
@@ -0,0 +1,18 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Infraestructura
+{
+    public static class BaseDeDatos
+    {
+        private static readonly string ConnectionString =
+            ConfigurationManager.ConnectionStrings["DATABASE"].ConnectionString;
+
+        public static SqlConnection ObtenerConexion()
+        {
+            var conn = new SqlConnection(ConnectionString);
+            conn.Open();
+            return conn;
+        }
+    }
+}
---
+++ UI/Listado.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace UI
+{
+    public partial class Listado : Form
+    {
+        public Listado()
+        {
+            InitializeComponent();
+        }
+    }
+}
